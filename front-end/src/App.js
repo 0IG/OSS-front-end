652455c6a30ff16fca9c4588bafc50de52c0bae9
Isaac Gonzalez <u1337x@gmail.com>
2023-07-09 17:27:01 -0400

    rightPanel bug fix attempt

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,15 +2,11 @@
 import { Route, Routes } from "react-router-dom";
 // Component Imports
 import NavBar from "./components/navBar/NavBar";
-import LeftPanel from "./components/leftPanel/LeftPanel";
-import RightPanel from "./components/rightPanel/RightPanel";
 import Home from "./components/Home/Home";
 import GearIndex from "./components/gearIndex/GearIndex";
 import ItemDetail from "./components/itemDetail/ItemDetail";
 import FourOFour from "./components/404/FourOFour";
 import Profile from "./components/accounts/Profile/Profile";
-import MiscIndex from "./components/gearIndex/miscIndex/MiscIndex";
-import WishList from "./components/accounts/wishList/WishList";
 import Cart from "./components/accounts/cart/Cart";
 import { CartProvider } from "./components/accounts/cart/CartContext";
 // Page Imports
@@ -30,12 +26,9 @@ export default function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/gear/:productId" element={<ItemDetail />}></Route>
         <Route path="/login" element={<Profile />}></Route>
-        {/* <Route path="/wishlist" element={<WishList />}></Route> */}
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="/404" element={<FourOFour />} />
         <Route path="/*" element={<FourOFour />} />
-        {/* We need GearShow to be able to have a custom filter for men/ women and sync to route. */}
-        {/* <Route path="/gear/mens" element={<GearShow />}></Route> */}
       </Routes>
     </CartProvider>
   );
--- front-end/src/components/rightPanel/rightPanel.js
+++ front-end/src/components/rightPanel/rightPanel.js
@@ -1,34 +1,34 @@
 import React from "react";
 import "./RightPanel.scss";
 
-export default function rightPanel() {
+export default function RightPanel() {
   return (
-    <div className="rightPanel">
-      <div className="rightPanel__sort">
-        <div className="rightPanel__sortTitle">SORT</div>
-        <div className="rightPanel__latest">Latest arrivals</div>
-        <div className="rightPanel__trending">Trending</div>
-        <div className="rightPanel__lowToHigh">Price: Low to high</div>
-        <div className="rightPanel__highToLow">Price: Hight to Low</div>
+    <div className="RightPanel">
+      <div className="RightPanel__sort">
+        <div className="RightPanel__sortTitle">SORT</div>
+        <div className="RightPanel__latest">Latest arrivals</div>
+        <div className="RightPanel__trending">Trending</div>
+        <div className="RightPanel__lowToHigh">Price: Low to high</div>
+        <div className="RightPanel__highToLow">Price: Hight to Low</div>
       </div>
 
-      <div className="rightPanel__colors">
-        <div className="rightPanel__colorsTitle">COLORS</div>
-        <div className="rightPanel__all">ALL COLORS</div>
-        <div className="rightPanel__black">Black</div>
-        <div className="rightPanel__blue">Blue</div>
-        <div className="rightPanel__brown">Brown</div>
-        <div className="rightPanel__burgundy">Burgundy</div>
-        <div className="rightPanel__gray">Gray</div>
-        <div className="rightPanel__green">Green</div>
-        <div className="rightPanel__navy">Navy</div>
-        <div className="rightPanel__orange">Orange</div>
-        <div className="rightPanel__pink">Pink</div>
-        <div className="rightPanel__purple">Purple</div>
-        <div className="rightPanel__red">Red</div>
-        <div className="rightPanel__tan">Tan</div>
-        <div className="rightPanel__white">White</div>
-        <div className="rightPanel__yellow">Yellow</div>
+      <div className="RightPanel__colors">
+        <div className="RightPanel__colorsTitle">COLORS</div>
+        <div className="RightPanel__all">ALL COLORS</div>
+        <div className="RightPanel__black">Black</div>
+        <div className="RightPanel__blue">Blue</div>
+        <div className="RightPanel__brown">Brown</div>
+        <div className="RightPanel__burgundy">Burgundy</div>
+        <div className="RightPanel__gray">Gray</div>
+        <div className="RightPanel__green">Green</div>
+        <div className="RightPanel__navy">Navy</div>
+        <div className="RightPanel__orange">Orange</div>
+        <div className="RightPanel__pink">Pink</div>
+        <div className="RightPanel__purple">Purple</div>
+        <div className="RightPanel__red">Red</div>
+        <div className="RightPanel__tan">Tan</div>
+        <div className="RightPanel__white">White</div>
+        <div className="RightPanel__yellow">Yellow</div>
       </div>
     </div>
   );
