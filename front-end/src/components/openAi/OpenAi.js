91b509677aacd093a1dfc471df96656568453f1a
Isaac Gonzalez <u1337x@gmail.com>
2023-10-25 14:39:09 -0400

    patching the leaked OpenApi key

--- front-end/src/components/openAi/OpenAi.js
+++ front-end/src/components/openAi/OpenAi.js
@@ -1,37 +1,37 @@
-// import React, { useState } from "react";
-// import { Configuration, OpenAIApi } from "openai";
-// import DDB from "./DDB/DDB";
+import React, { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import DDB from "./DDB/DDB";
 
-// const configuration = new Configuration({
-//   apiKey: "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",
-// });
+const configuration = new Configuration({
+  apiKey: process.env.SECRET_OPENAPI_KEY,
+});
 
-// const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);
 
-// const OpenAi = () => {
-//   const [aiMessage, setAiMessage] = useState("");
+const OpenAi = () => {
+  const [aiMessage, setAiMessage] = useState("");
 
-//   const sendReqAi = async () => {
-//     console.log("afsdf");
-//     const prompt = "What is a good ice cream place in New York?";
+  const sendReqAi = async () => {
+    console.log("afsdf");
+    const prompt = "What is a good ice cream place in New York?";
 
-//     const response = openai.createChatCompletion({
-//       model: "gpt-3.5-turbo",
-//       messages: [{ role: "user", content: prompt }],
-//     });
+    const response = openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    });
 
-//     setAiMessage("sdfsdfsdf"); //response.data.choices[0].message.content);
-//   };
+    setAiMessage("sdfsdfsdf"); //response.data.choices[0].message.content);
+  };
 
-//   return (
-//     <div className="openAi">
-//       {/* <button className="button-for-ai" onClick={() => sendReqAi()}>
-//         send req
-//       </button>
-//       {aiMessage} */}
-//       <DDB />
-//     </div>
-//   );
-// };
+  return (
+    <div className="openAi">
+      {/* <button className="button-for-ai" onClick={() => sendReqAi()}>
+        send req
+      </button>
+      {aiMessage} */}
+      <DDB />
+    </div>
+  );
+};
 
-// export default OpenAi;
+export default OpenAi;
